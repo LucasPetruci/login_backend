d645e463c23cefc7bf772c5bc23633b6de535e86
LucasPetruci <lucaspetruci100@gmail.com>
2024-12-27 17:55:57 -0300

    test: add failure cases for user registration in authController

--- src/tests/integration/authController.test.ts
+++ src/tests/integration/authController.test.ts
@@ -26,4 +26,35 @@ describe("Auth Controller", () => {
       );
     });
   });
+
+  //failure case
+  test("Should return 400 if email or password is missing", async () => {
+    // Arrange
+    const payload = { email: "test@example.com" };
+
+    // Act
+    const response = await request(app).post("/register").send(payload);
+
+    // Assert
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      message: "Email and password are required",
+    });
+  });
+
+  //failure case - error handle
+  test("Should return 400 if an error occurs", async () => {
+    // Arrange
+    const errorMesssage = "Registration failed";
+    (authService.register as jest.Mock).mockRejectedValue(
+      new Error(errorMesssage)
+    );
+    const payload = { email: "test@example.com", password: "123456" };
+
+    // Act
+    const response = await request(app).post("/register").send(payload);
+
+    // Assert
+    expect(response.status).toBe(400);
+  });
 });
