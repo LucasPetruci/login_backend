c96499107e994651e2c72baab0911532b42344a2
LucasPetruci <lucaspetruci100@gmail.com>
2024-12-27 18:05:06 -0300

    refactor: move server initialization to a new server.ts file and update dev script

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app: Application = express();
-const PORT = process.env.PORT;
 
 app.use(express.json());
-
 app.use("/", loginRouter);
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
 
 export default app;
