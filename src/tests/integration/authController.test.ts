e32036b1a7807c1b09c0a4d45e70c05c0fcc3933
LucasPetruci <lucaspetruci100@gmail.com>
2024-12-28 15:40:05 -0300

    test: add success and failure cases for user login in authController

--- src/tests/integration/authController.test.ts
+++ src/tests/integration/authController.test.ts
@@ -5,6 +5,7 @@ import * as authService from "../../services/authService";
 jest.mock("../../services/authService");
 
 describe("Auth Controller", () => {
+  //register
   describe("POST /register", () => {
     //success case
     test("Should register a user successfully", async () => {
@@ -56,5 +57,44 @@ describe("Auth Controller", () => {
 
     // Assert
     expect(response.status).toBe(400);
+  });
+});
+
+//login
+describe("POST /login", () => {
+  //success case
+  test("Should login a user successfully", async () => {
+    // Arrange
+    const mockResult = {
+      user: { id: 1, email: "test@example" },
+      token: "mocked_token_jwt",
+    };
+    (authService.login as jest.Mock).mockResolvedValue(mockResult);
+    const payload = { email: "test@example.com", password: "123456" };
+
+    // Act
+    const response = await request(app).post("/login").send(payload);
+
+    // Assert
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(mockResult);
+    expect(authService.login).toHaveBeenCalledWith(
+      payload.email,
+      payload.password
+    );
+  });
+
+  //failure case - invalid email or password
+  test("Should return 401 if invalid email or password", async () => {
+    // Arrange
+    (authService.login as jest.Mock).mockResolvedValue(null);
+    const payload = { email: "wrong@example.com", password: "wrong_password" };
+
+    // Act
+    const response = await request(app).post("/login").send(payload);
+
+    // Assert
+    expect(response.status).toBe(401);
+    expect(response.body).toEqual({ message: "Invalid email or password" });
   });
 });
